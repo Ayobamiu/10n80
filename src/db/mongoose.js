9c02c2df4fa72d922c4eca6468d9b958e7949532
Ayobamiu <ayobamiu@gmail.com>
2021-03-06 23:54:31 +0100

    npm start

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/10n80", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  "mongodb+srv://taskapp:tpt4nYro75w8Myjh@cluster0.bpapr.mongodb.net/app10n80?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
